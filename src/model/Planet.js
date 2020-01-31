bf6b3f8e3ec17490b41a310847fcda54b0461535
Leon Busse <leon.busse@br.de>
2020-01-31 22:52:58 +0100

    add model stubs

---
+++ src/model/Planet.js
@@ -0,0 +1,8 @@
+class Planet {
+  // position: {x: Number, y: Number};
+  // income: Number
+  // growthRate: Number
+  // spreadRate: Number
+  // population: {virus: Number, player: Number, default: Number}
+  // upgrades: {income: Number, growthRate: Number, spreadRate: Number }
+}
---
+++ src/model/SpaceConnection.js
@@ -0,0 +1,5 @@
+class SpaceConnection {
+  // start: {x: Number, y: Number}
+  // end: {x: Number, y: Number}
+  // sendPropbability: Number
+}
