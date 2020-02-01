d0e6a776f73d61f623c70f4e19531ba571c05f0d
Leon Busse <leon.busse@br.de>
2020-02-01 03:07:12 +0100

    upgrade planet attributes

--- src/ui/InfoArea.js
+++ src/ui/InfoArea.js
@@ -6,6 +6,9 @@ let selectedPopulation;
 let selectedGrowthRate;
 let selectedIncomeRate;
 let selectedSpreadRate;
+let selectedUpdateGrowth;
+let selectedUpdateIncome;
+let selectedUpdateSpread;
 
 export function setupInfoArea(scene) {
   var rect = new Phaser.Geom.Rectangle(
@@ -76,6 +79,36 @@ export function setupInfoArea(scene) {
       fontSize: 24
     }
   );
+
+  selectedUpdateGrowth = scene.add.text(
+    InfoArea.x + InfoArea.margin,
+    InfoArea.y + InfoArea.height / 2 + 300 + InfoArea.margin,
+    "Update Growth",
+    {
+      fontFamily: '"Roboto Condensed"',
+      fontSize: 18
+    }
+  );
+
+  selectedUpdateIncome = scene.add.text(
+    InfoArea.x + InfoArea.margin,
+    InfoArea.y + InfoArea.height / 2 + 340 + InfoArea.margin,
+    "Update Income",
+    {
+      fontFamily: '"Roboto Condensed"',
+      fontSize: 18
+    }
+  );
+
+  selectedUpdateSpread = scene.add.text(
+    InfoArea.x + InfoArea.margin,
+    InfoArea.y + InfoArea.height / 2 + 380 + InfoArea.margin,
+    "Update Spread",
+    {
+      fontFamily: '"Roboto Condensed"',
+      fontSize: 18
+    }
+  );
 }
 
 export function updateInfoArea(selectedObject, gameState) {
@@ -102,11 +135,23 @@ export function updateInfoArea(selectedObject, gameState) {
     selectedSpreadRate.setText(
       "Spread Rate: " + selectedObject.model.spreadRate
     );
+    selectedUpdateGrowth.setText(
+      "Update Growth - $" + selectedObject.model.upgrades.growthRate
+    );
+    selectedUpdateIncome.setText(
+      "Update Income - $" + selectedObject.model.upgrades.income
+    );
+    selectedUpdateSpread.setText(
+      "Update Spread  - $" + selectedObject.model.upgrades.spreadRate
+    );
   } else {
     selectedObjectTitle.setText("");
     selectedPopulation.setText("");
     selectedGrowthRate.setText("");
     selectedIncomeRate.setText("");
     selectedSpreadRate.setText("");
+    selectedUpdateGrowth.setText("");
+    selectedUpdateIncome.setText("");
+    selectedUpdateSpread.setText("");
   }
 }
