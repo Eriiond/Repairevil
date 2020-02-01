1799c7c037cbd44f85d5166f926f83552ec8c5ba
Leon Busse <leon.busse@br.de>
2020-02-01 02:28:56 +0100

    some info UI

--- src/model/GameLogic.js
+++ src/model/GameLogic.js
@@ -1,35 +1,35 @@
 const virusFactor = 0.25; // How fast the virus growth on planets;
-
+let counter = 0;
 export class GameLogic {
-
-    update(gameState) {
-        updateMoney(gameState);
-        updateShips(gameState);
-        updateFights(gameState);
+  static update(gameState) {
+    if (counter % 30 == 0) {
+      counter = counter + 1;
+      GameLogic.updateMoney(gameState);
+      GameLogic.updateShips(gameState);
+      GameLogic.updateFights(gameState);
     }
+  }
 
-    updateMoney(gameState) {
-        for (let i = 0; i < gameState.universe.planets.length; i++) {
-            const element = gameState.universe.planets[i];
-            if (element.population.player > 0) {
-                gameState.player.money += element.income;
-            }
-        }
+  static updateMoney(gameState) {
+    for (let i = 0; i < gameState.universe.planets.length; i++) {
+      const element = gameState.universe.planets[i];
+      if (element.population.player > 0) {
+        gameState.player.money += element.income;
+      }
     }
+  }
 
-    updateShips(gameState) {
-        for (let i = 0; i < gameState.universe.planets.length; i++) {
-            const element = gameState.universe.planets[i];
-            if (element.population.virus > 0) {
-                element.population.virus += virusFactor * (gameState.level % 5 + 1);
-            }
-            if (element.population.player > 0) {
-                gameState.player.money += element.income;
-            }
-        }
+  static updateShips(gameState) {
+    for (let i = 0; i < gameState.universe.planets.length; i++) {
+      const element = gameState.universe.planets[i];
+      if (element.population.virus > 0) {
+        element.population.virus += virusFactor * ((gameState.level % 5) + 1);
+      }
+      if (element.population.player > 0) {
+        gameState.player.money += element.income;
+      }
     }
+  }
 
-    updateFights(gameState) {
-
-    }
+  static updateFights(gameState) {}
 }
