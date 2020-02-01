d926e31149a77613576b10081ed73df21b775bfc
Leon Busse <leon.busse@br.de>
2020-02-01 16:22:32 +0100

    UI

--- src/ui/util.js
+++ src/ui/util.js
@@ -1,16 +1,37 @@
 export function shortenNumberText(number) {
-    if (Math.abs(number) > 999) {
-        const r1 = number / 1000
-        if (Math.abs(r1) > 999) {
-            return (
-                parseFloat(Math.sign(r1) * (Math.abs(r1) / 1000).toFixed(1)) +
-                "m"
-            )
-        } else {
-            return parseFloat(r1.toFixed(1)) + "k"
-        }
+    if (Math.abs(number) >= 1000000000000) {
+        return (
+            parseFloat(
+                Math.sign(number) *
+                    (Math.abs(number) / 1000000000000).toFixed(1)
+            ) + "T"
+        )
+    }
+
+    if (Math.abs(number) >= 1000000000) {
+        return (
+            parseFloat(
+                Math.sign(number) * (Math.abs(number) / 1000000000).toFixed(1)
+            ) + "B"
+        )
+    }
+
+    if (Math.abs(number) >= 1000000) {
+        return (
+            parseFloat(
+                Math.sign(number) * (Math.abs(number) / 1000000).toFixed(1)
+            ) + "M"
+        )
+    }
+
+    if (Math.abs(number) >= 1000) {
+        return (
+            parseFloat(
+                Math.sign(number) * (Math.abs(number) / 1000).toFixed(1)
+            ) + "k"
+        )
     } else {
-        return parseFloat(number.toFixed(1))
+        return parseFloat(number.toFixed(1)) + ""
     }
 }
 
