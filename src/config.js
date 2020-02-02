5f3e28e39d726223212c401ddc8a63a954b41ae0
Wolf Rost <Wolf.Rost@web.de>
2020-02-02 13:53:25 +0100

    Added sound on game start. Resized form in start screen.

--- src/config.js
+++ src/config.js
@@ -12,4 +12,7 @@ export default {
     dom: {
         createContainer: true,
     },
+    audio: {
+        disableWebAudio: true,
+    },
 };
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -70,9 +70,12 @@ export default class extends Phaser.Scene {
         this.load.image("glow_virus", "src/assets/glow_virus.png");
         this.load.image("glow_default", "src/assets/glow_default.png");
         this.load.image("rect", "src/assets/rect.png");
+
+        this.load.audio("intromusic", "src/assets/intromusic.mp3");
     }
 
     create() {
+        let music = this.game.sound.add("intromusic");
         const callbacks = {
             onA: () => this.onUpgradeGrowth(),
             onS: () => this.onUpgradeIncome(),
@@ -82,7 +85,6 @@ export default class extends Phaser.Scene {
             onSpaceUp: () => this.toggleSpaceConnections(),
         };
         this.inputManager = new InputManager(this, callbacks);
-
         this.setupUI();
         this.endGameText = this.add.text(
             (Viewport.width * 3) / 4 / 2,
@@ -127,6 +129,8 @@ export default class extends Phaser.Scene {
             );
             this.level = parseInt(levelInputElement.value);
             this.startLevel(this.level);
+
+            music.play();
         };
     }
 
